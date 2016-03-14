df848f0862ac8a5ca76605a7cb60066f4c1ce0c0
yordanterziev <yordan.terziev@paluno.uni-due.de>
2016-03-14 16:30:04 +0100

    computing the information content sample

--- slib-examples/src/main/java/slib/examples/sml/general/Swrc_Example.java
+++ slib-examples/src/main/java/slib/examples/sml/general/Swrc_Example.java
@@ -12,6 +12,10 @@ import slib.graph.model.graph.elements.E;
 import slib.graph.model.impl.graph.memory.GraphMemory;
 import slib.graph.model.impl.repo.URIFactoryMemory;
 import slib.graph.model.repo.URIFactory;
+import slib.sml.sm.core.engine.SM_Engine;
+import slib.sml.sm.core.metrics.ic.utils.IC_Conf_Topo;
+import slib.sml.sm.core.metrics.ic.utils.ICconf;
+import slib.sml.sm.core.utils.SMConstants;
 import slib.utils.ex.SLIB_Exception;
 
 public class Swrc_Example {
@@ -28,15 +32,28 @@ public class Swrc_Example {
         GDataConf graphconf = new GDataConf(GFormat.RDF_XML, fpath);
         GraphLoaderGeneric.populate(graphconf, graph);
         
+        SM_Engine engine = new SM_Engine(graph);
         
-        Set<E> e = graph.getE();
+        Set<URI> taxonomicLeaves = engine.getTaxonomicLeaves();
         
-        for (E e2 : e) {
-			System.out.println(e2);
-		}
+        
+        // First we define the information content (IC) we will use
+        ICconf icConf = new IC_Conf_Topo("RESNIK", SMConstants.FLAG_ICI_RESNIK_1995);
+        
+      
+        
+        System.out.println(engine.getIC(icConf, factory.getURI("http://swrc.ontoware.org/ontology#Event")));
+        
+        System.out.println(engine.getIC(icConf, factory.getURI("http://swrc.ontoware.org/ontology#Meeting")));
+        
+        System.out.println(engine.getIC(icConf, factory.getURI("http://swrc.ontoware.org/ontology#ProjectMeeting")));
+        
+//        for (URI uri : taxonomicLeaves) {
+//			System.out.println(uri);
+//		}
         
         // General information about the graph
-        System.out.println(graph.toString());
+        //System.out.println(graph.toString());
 	
 	}
 }
